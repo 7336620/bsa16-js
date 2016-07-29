61a18ae09afbc10e0380fd1a843d2cc29196f116
I'm Te <7336620@gmail.com>
2016-07-29 09:36:39 +0300

    0729 React Part2

--- 0729 React Part2/components/ulist/listView.js
+++ 0729 React Part2/components/ulist/listView.js
@@ -1,28 +1,68 @@
 import React from 'react';
 import ListItem from './listItem';
 
+// 3 variants of fast search
+
+// with filter , but not worked too (https://habrahabr.ru/post/229629/)
 class ListView extends React.Component {
 
-  /*render() {
-	const searchStr = this.props.text.trim() || '';
-    return (
-		(searchStr.length > 0)?
+	render() {
+        var libraries = this.props.users,
+            searchString = (this.props.text || '').trim().toLowerCase();
+
+
+        if(searchString.length > 0){
+
+            // We are searching. Filter the results.
+
+            libraries = libraries.filter(function(l){
+                return l.name.toLowerCase().match( searchString );
+            });
+
+        }
+		
+		return  <div className="list-view">
+					{ libraries.map(function(user) {
+					return <ListItem key={user.uniqueID} id={user.uniqueID} name={user.name} handleDelete={ this.props.handleDelete } />;
+						}, this) }
+				</div>
+    }
+}
+	
+/*
+// with filter , but not worked
+class ListView extends React.Component {
+
+  render() {
+	const searchStr = (this.props.text || '').trim();
+	if(searchStr.length > 0){
+		return (
 			<div className="list-view">
-				{this.props.users.map(function(user) {
-					((user.name).indexOf(searchStr) === 0) ? 
-						return( <ListItem key={user.uniqueID} id={user.uniqueID} name={user.name} handleDelete={ this.props.handleDelete } />)
-						: return( <hr />)		;
-				} )}			
-			</div> :
+				{this.props.users.map(function(user){
+						return( ((user.name).indexOf(searchStr) === 0) ? 
+							<ListItem key={user.uniqueID} id={user.uniqueID} name={user.name} handleDelete={ this.props.handleDelete } />
+							: <hr />);
+				},this )}			
+			</div> 
+		);		
+	}else{
+		return (
             <div className="list-view">
 				{this.props.users.map(function(user) {
 					return( <ListItem key={user.uniqueID} id={user.uniqueID} name={user.name} handleDelete={ this.props.handleDelete } />); 
-				} )}
+				}, this )}
 			</div>
-    );
-  }*/
-  
-    render() {
+		);		
+	}
+  }
+}
+ */
+ 
+ 
+//  working without filter 
+/*
+class ListView extends React.Component {  
+  render() {
     return (
 		<div className="list-view">{
 			this.props.users.map(function(user) {
@@ -32,7 +72,7 @@ class ListView extends React.Component {
 		</div>
     );
   }
-}
+}*/
 
 
 export default ListView;
